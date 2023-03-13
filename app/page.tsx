115fd77df7b241b1b010391c566408d63a22fd8c
Jane Dalton (nb0959) <nb0959@LM236JHYQG.local>
2023-03-13 14:24:12 +0000

    initial commit and basic  first working clock

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,62 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import React, { useState, useEffect } from "react";
+import styles from "./page.module.css";
+
+let hoursRotation: any = "0deg";
+let minsRotation: any = "0deg";
+let secsRotation: any = "0deg";
 
 export default function Home() {
+  const [time, setTime] = useState("");
+
+  const timeArray = time.split(":");
+  const hours = parseInt(timeArray[0]);
+  const mins = parseInt(timeArray[1]);
+  const secs = parseInt(timeArray[2]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let now = new Date().toLocaleTimeString();
+      setTime(now);
+
+      secsRotation = 360 * (secs / 60) + "deg";
+      minsRotation = 360 * (mins / 60) + "deg";
+      hoursRotation = 360 * (hours / 12) + "deg";
+    }, 1000);
+  });
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1>Clock</h1>
+      <p>{timeArray}</p>
+      <p>
+        {hours} {mins} {secs}
+      </p>
+      <p>
+        {hoursRotation} {minsRotation} {secsRotation}
+      </p>
+      <div id="clock">
+        <div
+          className="minute_container clockface"
+          style={{ ["rotate" as string]: minsRotation }}
+        >
+          <div className="minute_hand"></div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div
+          className="hour_container clockface"
+          style={{ ["rotate" as string]: hoursRotation }}
+        >
+          <div className="hour_hand"></div>
         </div>
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className="seconds_container clockface"
+          style={{ ["rotate" as string]: secsRotation }}
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="second_hand"></div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
